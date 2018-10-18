1d07bce635461170ddec8e5f0fc0a82b8115c5a1
Adefemi Kolawole <akolawole@ggc.edu>
2018-10-18 18:10:33 -0400

    created a default text writer method that can be caussed anytime to write text

---
+++ src/Tester.java
@@ -0,0 +1,15 @@
+
+public class Tester {
+	static String username;
+	 static String password;
+	public static void main(String [] args) {
+		Writer writer = new Writer();
+		username= "akolawole";
+		password = "123456";
+		
+		
+		//writer.write("username", username );
+		writer.write("password", password );
+	}
+
+}
