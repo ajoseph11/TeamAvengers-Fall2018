4149c6d8014a6317cc672e356b5dc8ce7ae7e535
Adefemi Kolawole <akolawole@ggc.edu>
2018-10-19 12:06:38 -0400

    Gui for the game started

---
+++ src/gui/GUI.java
@@ -0,0 +1,57 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Label;
+
+public class GUI extends Application{
+   final private int windowWidth = 1400; // width of Window
+    final private int windowHeight = 900; //height of window
+   VBox vbCenter; // vertical pane in the center of the window's borderPane
+   VBox vbLeft;  // vertical pane in the left  of the window's borderPane
+   VBox vbRight;  // vertical pane in the right of the window's borderPane
+   HBox hbTop;  // horizontal pane in the top of the window's borderPane
+   HBox hbBottom; // horizontal pane in the bottom of the window's borderPane
+   Label lbTitle; // label for game title
+   Insets insets; // insets to create margin or padding aroung node items
+   BorderPane borderPane; // main border Pane to display the window
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+		insets = new Insets(5,5, 5,5); //insets to set margin to nodes
+		//Button btViewInstr = new Button("Create Account");
+		//Button btLaunchCM = new Button("Create Account");
+		lbTitle = new Label("WELCOME TO CITADEl OF STORMS");
+		hbTop = new HBox();
+		hbTop.getChildren().add(lbTitle);
+		hbTop.setAlignment(Pos.CENTER);
+		
+		borderPane = new BorderPane();
+		borderPane.setTop(hbTop);
+		borderPane.setCenter(vbCenter);
+		borderPane.setLeft(vbLeft);
+		borderPane.setRight(vbRight);
+		borderPane.setBottom(hbBottom);
+		
+		
+		Scene scene = new Scene(borderPane, windowWidth, windowHeight); // set size of the windwo
+		primaryStage.setTitle("Citadel_Of_Storms");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		primaryStage.setResizable(false); // disallow increasing window size
+		
+	}
+	
+	public static void main(String [] args) {
+		Application.launch(args);
+	}
+
+}
